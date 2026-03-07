5d846b2acaf5be0f0d914a4b7fa97f9d9ee1928e
Cade Quigley <caquigley@alaska.edu>
2026-03-07 08:25:09 -0800

    Added fk function, some more cleaning up. Also updated array_functions to most recent version

--- Lab6/multiple_events_array.py
+++ Lab6/multiple_events_array.py
@@ -23,16 +23,31 @@ from datetime import datetime
 import lts_array
 
 #Functions needed fro code----------------------------
-from array_functions import data_from_inventory
-from array_functions import get_geometry
-from array_functions import pull_earthquakes
-from array_functions import check_num_stations
-from array_functions import stations_available_generator
-from array_functions import array_time_window
-from array_functions import moveout_time
-from array_functions import grab_preprocess
-from array_functions import least_squares
-from array_functions import triggers
+from array_functions import (data_from_inventory, get_geometry, pull_earthquakes,
+                             check_num_stations, stations_available_generator,
+                             array_time_window, moveout_time, grab_preprocess,
+                             least_trimmed_squares, triggers, fk_obspy)
+from array_figures import baz_error_spatial, slow_error_spatial
+
+
+'''
+Conducts array analysis for a set array and number of events in the vicinity
+of the array. This can be used to determine how well an array is performing,
+and how errors are occuring spatially.
+    
+Parameters:
+    use_full_deployment: whether to use full time window deployment was out (True or False)
+    start_d1_list: list of start times for each station
+    end_d1_list: list of end times for each station
+    starttime: specified starttime, will use if use_full_deployment = True
+    endtime: speficied endtime, will use if use_full_deployment = True
+
+Returns: 
+    df: dataframe containing earthquake information, array output parameters,
+    plots: baz error, slowness error
+        
+        
+    '''
 
 #from array_functions import rotate_data
 
@@ -42,27 +57,27 @@ from array_functions import triggers
 
 #station/array inputs----------
 net = '9C' #9C, 4E, 5E, UW, XG
-sta = '3A**' #'2A*', '3A*', 'POM*', S1**, UIL*, LC*, IL*
+sta = '2A*' #'2A*', '3A*', 'POM*', S1**, UIL*, LC*, IL*
 loc = '*' #0
 chan = 'SHZ' #SHZ, DHZ, HHZ
 client = 'IRIS' #IRIS, GEOFON, path, #if 'path', create new variable path =
 starttime = '2015-10-01' #'2015-10-01' , '2011-05-11' '2025-09-07'
-endtime = '2015-10-15'#'2015-10-02' , '2013-05-01', '2025-11-13'
-min_stations = 10 # if you only want times with all stations, list the number of stations
-remove_stations =  ['3A10', '3A15'] #['POM06', 'POM07', 'POM18'] #['3A10', '3A15'] # ['POM06', 'POM07', 'POM18']
+endtime = '2015-10-12'#'2015-10-02' , '2013-05-01', '2025-11-13'
+min_stations = 8 # if you only want times with all stations, list the number of stations
+remove_stations =  []#['3A10', '3A15'] #['POM06', 'POM07', 'POM18'] #['3A10', '3A15'] # ['POM06', 'POM07', 'POM18']
 keep_stations = [] #****NOT WORKING YET****
-array_name = '3A' #2A, 3A, POM, KD, HM, S1, UIL
-use_full_deployment = False
-path_to_inventory = None
-save = False
+array_name = '2A' #2A, 3A, POM, KD, HM, S1, UIL
+use_full_deployment = False #if True, searches for full deployment length in inventory and finds all events
+path_to_inventory = None #if inventory object is stored locally
+save = False #save the dataframe to CSV or not
 
 #Earthquake inputs----------
 min_mag = '3.0' #minimum magnitude
-max_rad = '400' #maximum radius from arrays
+max_rad = '600' #maximum radius from arrays
 velocity_model = 'iasp91' #iasp91, pavdut, scak, ak135
 
 #Array processing---------------
-processing = 'lts' #ls, fk, lts
+processing = 'fk' #ls, fk, lts
 FREQ_MIN = 0.5
 FREQ_MAX = 18.0
 WINDOW_LENGTH = 2.5 #seconds
@@ -74,10 +89,10 @@ window_start = -1 #1 second before trigger
 
 # Inputs for sta/lta-------
 
-timing = 'trigger' #'power', 'trigger'
-min_triggers = 3 #minimum station triggers to associate
+timing = 'trigger' #'power', 'trigger', NEED TO FIX POWER
+min_triggers = min_stations // 3 #minimum station triggers to associate
 ptolerance = 5 #seconds, +/- around p-arrival
-multiple_triggers = 'peak' #'closest', 'peak'
+multiple_triggers = 'peak' #'closest', 'peak', which trigger to choose if multiple
 
 #Inputs for FK array processing---------
 
@@ -103,12 +118,13 @@ prewhiten = 0
 if client == 'path':
     inv = read_inventory(path_to_inventory) #need to add something at some point about
 else:
+    #client = Client(client, user="caquigley@alaska.edu", password="U9sWxXLREK4FsdUX", debug = True)
     client = Client(client)
+
 
     inv = client.get_stations(network=net, station=sta, channel=chan,
                                 location=loc, starttime=UTCDateTime(starttime),
                                 endtime=UTCDateTime(endtime), level='response') #level = 'channel'
-
 #Pull station information out of inventory
 (lat_list, lon_list, elev_list, station_d1_list,
  start_d1_list, end_d1_list, num_channels_d1_list) = data_from_inventory(inv, 
@@ -192,108 +208,44 @@ for event in range(len(df)):
                                                net, loc, chan, min_stations, 
                                                START, END, FREQ_MIN, FREQ_MAX,
                                                client)
+        
+        st1 = st.copy() #Pulling this out for FK
 
         ###Finding triggers---------------------------------
         if timing == 'trigger': #use sta/lta triggers
             (st, trigger, peak, length, 
-             trigger_type, trigger_time)= triggers(st, moveout, min_triggers, 
+             trigger_type, trigger_time, 
+             START_new, END_new)= triggers(st, moveout, min_triggers, 
                                                    ptolerance, START, 
                                                    window_start, 
                                                    WINDOW_LENGTH, 
                                                    multiple_triggers)
-            print(trigger_type)
+    
                 
 
         ###Array processing---------------------------------
         ##Least squares--------------------
         if processing == 'lts' or processing == 'ls':
 
-            array_data = least_squares(processing, st, sta_lats, sta_lons, 
+            array_data = least_trimmed_squares(processing, st, sta_lats, sta_lons, 
                                        WINDOW_LENGTH, WINDOW_OVERLAP,
                                        trigger_time, trigger_type, peak,
                                        length, origin_lat, origin_lon, 
                                        event_id, eq_baz_real, eq_slow_real)
             
-
         ##Frequency wavenumber--------------------
-        else: #fk analysis
-            ####IM IGNORING THE FK STUFF NOW AND JUST FOCUSING ON LTS
-
-            print('Starting FK')
-            #Add necessary data to streams----------------
-            for l in range(len(stations)):  # Uses all stations in pd dataframe stations
-                st1[l].stats.coordinates = AttribDict({
-                    'latitude': sta_lats[l],
-                    'elevation': sta_elev[l],
-                    'longitude': sta_lons[l]})
-    
-            kwargs = dict(
-                # slowness grid: X min, X max, Y min, Y max, Slow Step
-                sll_x=sll_x, slm_x=slm_x, sll_y=sll_y, slm_y=slm_y, sl_s=sl_s,
-                # sliding window properties
-                win_len=WINDOW_LENGTH, win_frac=WINDOW_OVERLAP,
-                # frequency properties
-                frqlow=FREQ_MIN, frqhigh=FREQ_MAX, prewhiten=prewhiten,
-                # restrict output
-                semb_thres=semb_thres, vel_thres=vel_thres, timestamp=timestamp,
-                stime=START+0.2, etime=END-0.2 #had to add and subtract to avoid timing errors
-                )
-            out = array_processing(st1, **kwargs)
-    
-            #OUTPUT FROM FK PROCESSING-----------------------------------------------------
-            array_out = pd.DataFrame(out, columns = ['time','relpow','abspow','baz_obspy','array_slow'])
-        
-
-            t = array_out['time'].to_numpy()
-            baz_obspy = array_out['baz_obspy'].to_numpy()
-        
-            bazs = []
-            time_error = []
-            for j in range(len(t)):
-                matplotlib_time = t[j]
-                x = mdates.num2date(matplotlib_time) 
-                x = UTCDateTime(x)
-                #diff = (x-UTCDateTime(time_station))+(win_len/2)
-                diff = str(x+(WINDOW_LENGTH/2)) #time centered on point
-                time_error.append(diff)
-                baz = baz_obspy[j]
-                if baz <= 0:
-                    baz_correct = baz+360 #converts to all positive backazimuth
-                else:
-                    baz_correct = baz
-                bazs.append(baz_correct)
-        
-            time_error = np.array(time_error)
-            fk_bazs = np.array(bazs)
-
-            fk_baz_error = baz_error(eq_baz[event], fk_bazs)
-
-            trace_vel_error = (1/eq_slow[event])- 1/array_out['array_slow'].to_numpy() #real - array
-
-            slowness_error = (eq_slow[event]) - array_out['array_slow'].to_numpy()
-
-            array_out['baz_error'] = fk_baz_error
-            array_out['centered_time'] = time_error
-            array_out['array_baz'] = fk_bazs
-            array_out['slow_error'] = slowness_error
-
-            #Pull out greatest power------------
-            idx = np.argmax(array_out['relpow'].to_numpy())
-        
-            array_data = array_out.loc[[idx]]
-            array_data['event_id'] = event_ids[event]
-            array_data['trigger_type'] = trigger_type
-            array_data['trigger_time'] = str(START+trigger)
-
-
-
+        else: 
+            array_data = fk_obspy(st1, stations, sta_lats, sta_lons, sta_elev, START, START_new, END_new,
+                                  WINDOW_LENGTH, WINDOW_OVERLAP, FREQ_MIN, FREQ_MAX,
+                                  sll_x, slm_x, sll_y, slm_y, sl_s, semb_thres, vel_thres, timestamp, prewhiten,
+                                  eq_baz_real, eq_slow_real, event_id, trigger, trigger_type, peak, length, origin_lat, origin_lon)
 
 
     ################################################################      
     
         array_data_list.append(array_data)
 
-        print('Events completed:', str(event)+'/'+str(len(df)))
+        print('Events completed:', str(event+1)+'/'+str(len(df)))
 
     except ValueError as e:
         print(f"Skipping event {event_ids[event]}: {e}")
@@ -310,6 +262,16 @@ array_data_comb1 = pd.concat(array_data_list, ignore_index=True)
 array_data_comb = pd.merge(array_data_comb1, df, on='event_id', how='inner')
 
 if save == True:
-    array_data_comb.to_csv(array_name+'_400km_m3_.csv')
+    array_data_comb.to_csv(array_name+'_'+max_rad+'km_m3_fk_wawa.csv')
 
+#Plot some figures
+df = array_data_comb
+color_data = df['distance']
+color_label = 'earthquake distance (km)'
+model_data = []
+baz_error_spatial(df['backazimuth'], df['baz_error'], model_data, color_data, color_label, niazi = True,
+                  save = False, path = '/Users/cadequigley/Downloads/Research/gigls_2026/POM_baz_error_spatial.png')
+
+slow_error_spatial(df['backazimuth'], df['slow_error'], model_data, color_data, color_label, niazi = True, 
+                   save = False, path = '/Users/cadequigley/Downloads/Research/gigls_2026/POM_slow_error_spatial.png')
 
